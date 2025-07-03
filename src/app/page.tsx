3700dcbfe941fbf59ca12515651cb3afb5f80f94
lennonconstantino <lennonconstantino@gmail.com>
2025-07-03 16:34:41 -0300

    input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import { GameProps } from '@/utils/types/game'
 import Image from "next/image";
 import Link from 'next/link'
 
+import { BsArrowRightSquare } from 'react-icons/bs'
+import { Input } from '@/components/input'
+
 
 async function getDalyGame(){
   try{
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { next: { revalidate: 320 } })
     return res.json();
   }catch(err){
     throw new Error("Failed to fetch data")
@@ -21,19 +24,34 @@ export default async function Home() {
   return (
     <main className="w-full">
       <Container>
-        <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo pra você</h1>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+        {/* <h1 className="bg-red-500 text-white text-center font-bold text-xl mt-8 mb-5 p-4"> */}
+          Separamos um jogo exclusivo pra você
+        </h1>
         <Link href={`/game/${dalyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image
-              src={dalyGame.image_url}
-              alt={dalyGame.title}
-              priority={true}
-              quality={100}
-              width={100}
-              height={100}
-            />
+            <div className="w-full max-h-96 h-96 relative rounded-lg overflow-hidden">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                // fill={true}
+                width={800}
+                height={400}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+              />
+            </div>
           </section>
         </Link>
+
+        <Input/>
 
       </Container>
     </main>
