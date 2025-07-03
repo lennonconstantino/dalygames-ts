9f8a6f02f381555e2e9062680cfe0a6008be97ae
lennonconstantino <lennonconstantino@gmail.com>
2025-07-03 18:23:13 -0300

    creating detail page final

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -2,13 +2,23 @@ import { GameProps } from "@/utils/types/game";
 import { redirect } from "next/navigation";
 import Image from 'next/image'
 import { Container } from '@/components/container'
+import { Label } from './components/label'
+import { GameCard } from "@/components/GameCard";
 
 
 async function getData(id: string) {
-  //https://sujeitoprogramador.com/next-api/?api=game&id=15
-
   try {
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { next: { revalidate: 60 } })
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data")
+  }
+}
+
+async function getGameSorted() {
+  //https://sujeitoprogramador.com/next-api/?api=game_day
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: "no-store" })
     return res.json();
   } catch (err) {
     throw new Error("Failed to fetch data")
@@ -22,6 +32,9 @@ export default async function Game({
   params: { id: string }
 }) {
   const data: GameProps = await getData(id)
+  const sortedGame: GameProps = await getGameSorted();
+
+
 
   if (!data) {
     redirect("/")
@@ -44,6 +57,29 @@ export default async function Game({
       <Container>
         <h1 className="font-bold text-xl my-4">{data.title}</h1>
         <p>{data.description}</p>
+
+        <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+        <div className="flex gap-2 flex-wrap">
+          {data.platforms.map((item) => (
+            <Label name={item} key={item} />
+          ))}
+        </div>
+
+        <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
+        <div className="flex gap-2 flex-wrap">
+          {data.categories.map((item) => (
+            <Label name={item} key={item} />
+          ))}
+        </div>
+
+        <p className="mt-7 mb-2"><strong>Data de lançamento:</strong> {data.release}</p>
+
+        <h2 className="font-bold text-lg mt-7 mb-2">Jogo recomendado:</h2>
+        <div className="flex">
+          <div className="flex-grow">
+            <GameCard data={sortedGame} />
+          </div>
+        </div>
 
       </Container>
     </main>
